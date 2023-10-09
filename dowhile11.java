59968a8e7fc2862e68c048299eaf7fb860ed6feb
FarhanMawaludin <farhanmawaludin7@gmail.com>
2023-10-09 15:21:13 +0700

    modif dowhile11

--- dowhile11.java
+++ dowhile11.java
@@ -1,6 +1,6 @@
 public class dowhile11 {
     public static void main(String[] args) {
-        int x = 0;
+        int x = 10;
         do {
             System.out.println(x);
         }while (++x <= 8);
