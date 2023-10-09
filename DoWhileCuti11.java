f4f58999c85f883c405ec4706255e754b724f2fd
FarhanMawaludin <farhanmawaludin7@gmail.com>
2023-10-09 13:47:19 +0700

    do while cuti

---
+++ DoWhileCuti11.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class DoWhileCuti11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int jatahCuti, jumlahHari;
+        String konfirmasi;
+
+        System.out.print("Jatah Cuti : ");
+        jatahCuti = sc.nextInt();
+
+        do {
+            System.out.print("Apakah Anda Ingin mengambil cuti ? ");
+            konfirmasi = sc.next();
+
+            if (konfirmasi.equalsIgnoreCase("y")){
+            System.out.print("Jumlah Hari : ");
+            jumlahHari = sc.nextInt();
+
+            if((jumlahHari <= jatahCuti)) {
+                jatahCuti -= jumlahHari;
+                System.out.println("Sisa jatah cuti " + jatahCuti);
+            }else {
+                System.out.println("Sisa jatah cuti anda tidak ada");
+                break;
+            }
+            }
+        }while(jatahCuti > 0);
+
+        
+    }
+}
