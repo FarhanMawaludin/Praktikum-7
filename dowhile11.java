5af11731fc0db0dd1ef2384c3a84ce364356b9c0
FarhanMawaludin <farhanmawaludin7@gmail.com>
2023-10-09 15:19:46 +0700

    dowhile11

---
+++ dowhile11.java
@@ -0,0 +1,9 @@
+public class dowhile11 {
+    public static void main(String[] args) {
+        int x = 0;
+        do {
+            System.out.println(x);
+        }while (++x <= 8);
+        System.out.println("Program Berakhir");
+    }
+}
